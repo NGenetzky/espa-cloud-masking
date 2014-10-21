08431d3e253f0f9dfd8c88bcbbe28768a0fde5d5
Ronald Dilley <rdilley@usgs.gov>
2014-10-21 21:29:22 +0000

    code cleanup
    git-svn-id: https://cfmask.googlecode.com/svn/cfmask/trunk@215 8aa44e39-407d-e975-90f4-c7438b917e6c

--- src/split_filename.c
+++ src/split_filename.c
@@ -12,29 +12,27 @@ Date        Programmer       Reason
 --------    ---------------  -------------------------------------
 3/15/2013   Song Guo         Modified from LDCM IAS code
 ******************************************************************************/
-#include <string.h>
-#include <limits.h>         /* For PATH_MAX */
-#include "error.h"
-#include "input.h"
-#include "const.h"
 
-void split_filename 
+#include <string.h>
+#include <limits.h>
+
+void split_filename
 (
-    const char *filename,       /* I: Name of file to split */
-    char *directory,            /* O: Directory portion of file name */
-    char *scene_name,           /* O: Scene name portion of the file name */
-    char *extension             /* O: Extension portion of the file name */
+    const char *filename, /* I: Name of file to split */
+    char *directory,      /* O: Directory portion of file name */
+    char *scene_name,     /* O: Scene name portion of the file name */
+    char *extension       /* O: Extension portion of the file name */
 )
 {
-    char file_name[PATH_MAX];      /* Local copy of filename */
-    char *ptr = NULL;              /* String pointer */
+    char file_name[PATH_MAX]; /* Local copy of filename */
+    char *ptr = NULL;         /* String pointer */
 
     /* Make a local copy of filename so it is not destroyed */
-    strcpy(file_name, filename);
+    strcpy (file_name, filename);
 
     /* Check for a directory path */
     /* Find ending '/' */
-    ptr = (char *) strrchr(file_name, '/');
+    ptr = (char *) strrchr (file_name, '/');
     if (ptr != NULL)
     {
         *(ptr++) = '\0';
@@ -44,8 +42,8 @@ void split_filename
         strcpy (directory, "");
 
     /* Check for a file extension */
-    strcpy(file_name, filename);
-    ptr = (char *) strrchr(file_name, '.');
+    strcpy (file_name, filename);
+    ptr = (char *) strrchr (file_name, '.');
     if (ptr != NULL)
     {
         ptr++;
@@ -55,8 +53,8 @@ void split_filename
         strcpy (extension, "");
 
     /* Obtain the scene name */
-    strcpy(file_name, filename);
-    ptr = (char *) strrchr(file_name, '.');
+    strcpy (file_name, filename);
+    ptr = (char *) strrchr (file_name, '.');
     if (ptr != NULL)
     {
         *(ptr++) = '\0';
@@ -64,6 +62,4 @@ void split_filename
     }
     else
         strcpy (scene_name, file_name);
-
-    return;
 }
